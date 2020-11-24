1998a1a40e3e0aff170116c94d5a752cfdb98b44
MadNit <53405149+MadNit@users.noreply.github.com>
2020-11-24 22:55:22 +0530

    classes

--- classes.py
+++ classes.py
@@ -8,6 +8,10 @@ print('Defining a class')
 class myclass:
     def __init__(self):
         self.data = 10
+        self.st = "myclass function"
+        self.dec = 13.6
+        self.myl = []
+        self.myd = {"1": 1, "2": 2}
     def display(self):
         print(f"Printing the value of data: {self.data}")
     def cal(self, val):
@@ -16,3 +20,37 @@ class myclass:
 m1 = myclass()
 m1.display()
 m1.cal(3)
+
+class animal:
+    # This the constructor method and will be called
+    # during a instance creation.
+    def __init__(self, carn, tail, legs, wild):
+        self.carnivorous = carn
+        self.tail = tail
+        self.legs = legs
+        self.wild = wild
+        self.name = None
+        self.set_animal()
+
+    def set_animal(self):
+        if self.carnivorous == True and self.wild == True:
+            self.name = "Tiger"
+
+    def print_name(self):
+        print(f"The animal is {self.name}")
+
+
+tiger = animal(True, True, 4, True)
+tiger.print_name()
+
+class cat(animal):
+    def __init__(self, carn, tail, legs, wild):
+        super().__init__(carn, tail, legs, wild)
+        self.canswim = False
+        self.pet = None
+    def print(self):
+        pass
+
+
+mycat = cat(True, True, 4, False)
+print(mycat.wild)
--- functions.py
+++ functions.py
@@ -15,3 +15,10 @@ def multiply(x, y):
 
 mulnum = multiply(150, 160)
 print(f"The return value is {mulnum}")
+
+# Demonstrating lambda functions
+# Lambda functions are anonymous functions
+x = lambda a: a + 10
+print(f'The value of x is {x}')
+y = x(5)
+print(f'The value of y is {y}')
